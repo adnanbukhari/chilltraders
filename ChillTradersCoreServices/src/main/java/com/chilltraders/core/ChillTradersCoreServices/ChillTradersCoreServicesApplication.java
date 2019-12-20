aee2936cff7651e223617c22402ec4cb609b8818
Adnan Bukhari <BukhariA@CalACES.org>
2019-12-19 16:39:31 -0800

    Initial Version

---
+++ ChillTradersCoreServices/src/main/java/com/chilltraders/core/ChillTradersCoreServices/ChillTradersCoreServicesApplication.java
@@ -0,0 +1,13 @@
+package com.chilltraders.core.ChillTradersCoreServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChillTradersCoreServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChillTradersCoreServicesApplication.class, args);
+	}
+
+}
---
+++ ChillTradersCoreServices/src/test/java/com/chilltraders/core/ChillTradersCoreServices/ChillTradersCoreServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chilltraders.core.ChillTradersCoreServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChillTradersCoreServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
